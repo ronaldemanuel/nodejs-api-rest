9ca17a3c42d3e11a6b98699618cec8212f6de77e
ronaldemanuel <emronald15el@gmail.com>
2021-04-21 21:33:24 -0300

    Integrando ao banco de dados mysql e inicializando conexao

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const customExpress = require('./config/customExpress')
+const conexao = require('./database/conexao')
 
-const app = customExpress()
+conexao.connect((erro) => {
+    if (erro) {
+        console.log(erro)
+    } else {
+        console.log('Conectado com mysql')
+        
+        const app = customExpress()
 
-app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
+        app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
+    }
+})
+
+
 
