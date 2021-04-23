5d9a40314b751aed6a2ca0a43ef9c9c0bfff8d7a
ronaldemanuel <emronald15el@gmail.com>
2021-04-22 23:11:46 -0300

    Recurso de data adicionado

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,10 +1,15 @@
+const moment = require('moment')
 const connection = require('../database/connection')
 
 class Atendimento {
     adiciona(atendimento) {
+        const data_criacao = new Date()
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        const atendimentoDatado = {...atendimento, data_criacao, data}
+        
         const sql = 'INSERT INTO Atendimentos SET ?'
 
-        connection.query(sql, atendimento, (error, results) => {
+        connection.query(sql, atendimentoDatado, (error, results) => {
             if (error) {
                 console.log(error)
             } else {
