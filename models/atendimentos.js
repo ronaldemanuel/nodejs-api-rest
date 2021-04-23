1502041bdd6ac59af21ef31bae3e2cec15b9dd18
ronaldemanuel <emronald15el@gmail.com>
2021-04-22 23:30:02 -0300

    Validando erros

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -2,20 +2,44 @@ const moment = require('moment')
 const connection = require('../database/connection')
 
 class Atendimento {
-    adiciona(atendimento) {
+    adiciona(atendimento, res) {
         const data_criacao = new Date()
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
-        const atendimentoDatado = {...atendimento, data_criacao, data}
         
-        const sql = 'INSERT INTO Atendimentos SET ?'
-
-        connection.query(sql, atendimentoDatado, (error, results) => {
-            if (error) {
-                console.log(error)
-            } else {
-                console.log(results)
+        const dataValida = moment(data).isSameOrAfter(data_criacao)
+        const clienteValido = atendimento.cliente.length >= 5
+        
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteValido,
+                mensagem: 'Cliente deve ter pelo menos cinco caracteres'
             }
-        })
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido)
+        const existemErros = erros.length
+
+        if (existemErros) {
+            res.json(erros)
+        } else {
+            const atendimentoDatado = {...atendimento, data_criacao, data}
+            
+            const sql = 'INSERT INTO Atendimentos SET ?'
+    
+            connection.query(sql, atendimentoDatado, (error, results) => {
+                if (error) {
+                    console.log(error)
+                } else {
+                    console.log(results)
+                }
+            })
+        }
     }
 }
 
