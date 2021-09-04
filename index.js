7d0fa9a1e2795632dbba61662277b37829ab2e3d
ronaldemanuel <emronald15el@gmail.com>
2021-09-04 12:16:00 -0300

    chore: server code refactoring

--- index.js
+++ index.js
@@ -2,6 +2,4 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.listen(PORT, () => {
-    console.log('Server running on PORT: ' + PORT);
-});
+app.listen(PORT, () => console.log('Server running on PORT: ' + PORT));
