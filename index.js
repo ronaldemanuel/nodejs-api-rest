5e3eb6e824f6ca4f0afe0f0485ebf22661177789
ronaldemanuel <emronald15el@gmail.com>
2021-09-04 12:33:55 -0300

    feat: install and config consign

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express');
-const app = express();
+const consign = require('consign');
 const PORT = 3000;
+
+const app = express();
+
+consign()
+    .include('controllers')
+    .into(app)
 
 app.listen(PORT, () => console.log('Server running on PORT: ' + PORT));
