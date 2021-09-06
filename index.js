8a63342158ef8c07e4b282b349cb65ce143829c8
ronaldemanuel <emronald15el@gmail.com>
2021-09-05 21:17:40 -0300

    feat: organize route system with controllers and consign

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const customExpress = require('./config/customExpress');
+const consign = require('consign');
 const PORT = 3000;
 
 const app = customExpress()
 
+consign()
+    .include('controllers')
+    .into(app)
 
 app.listen(PORT, () => console.log('Server running on PORT: ' + PORT));
