0e799cf15d0e9e4e4b0ffc6de0f67bef37a2704b
ronaldemanuel <emronald15el@gmail.com>
2021-09-06 09:34:41 -0300

    feat: create connection on index.js

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const customExpress = require('./config/customExpress');
-const consign = require('consign');
+const conn = require('./infrastructure/database/connection');
 const PORT = 3000;
 
-const app = customExpress()
-
-app.listen(PORT, () => console.log('Server running on PORT: ' + PORT));
+conn.connect((err) => {
+    if (err) {
+        console.log('Connection fail: ' + err);
+    } else {
+        console.log('Connected database');
+        
+        const app = customExpress();
+        
+        app.listen(PORT, () => console.log('Server running on PORT: ' + PORT));
+    }
+});
