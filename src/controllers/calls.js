f59d6c8f4c25f606d9b134fba69c522942cee6dd
ronaldemanuel <emronald15el@gmail.com>
2021-09-06 15:13:58 -0300

    feat: implement store function on controller

--- src/controllers/calls.js
+++ src/controllers/calls.js
@@ -1,8 +1,14 @@
+const Calls = require('../models/calls');
+
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um GET'));
 
     app.post('/atendimentos', (req, res) => {
         console.log(req.body);
+
+        const call = req.body;
+        Calls.store(call);
+
         res.send('Você está na rota de atendimentos e está realizando um POST')
     });
 }
