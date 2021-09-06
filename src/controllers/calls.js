ef2813dbf251763624a84f85afde5f5caa93e21f
ronaldemanuel <emronald15el@gmail.com>
2021-09-06 16:19:21 -0300

    chore: refactore calls post route responses

--- src/controllers/calls.js
+++ src/controllers/calls.js
@@ -4,11 +4,7 @@ module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um GET'));
 
     app.post('/atendimentos', (req, res) => {
-        console.log(req.body);
-
         const call = req.body;
-        Calls.store(call);
-
-        res.send('Você está na rota de atendimentos e está realizando um POST')
+        Calls.store(call ,res);
     });
 }
