dbedd99d90de4baa2734d70a0abf2996301e2896
ronaldemanuel <emronald15el@gmail.com>
2021-09-06 16:41:07 -0300

    chore: implement store validations

--- src/models/calls.js
+++ src/models/calls.js
@@ -6,18 +6,42 @@ class Calls {
         const created_at = moment().format('YYYY-MM-DD HH:MM:SS');
         const date = moment(requestCall.date, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
 
-        console.log(created_at);
         const call = {...requestCall, created_at, date};
-        
-        const sql = 'INSERT INTO calls SET ?;';
 
-        conn.query(sql, call, (err, results) => {
-            if (err) {
-                res.status(400).json(err);
-            } else {
-                res.status(201).json(results);
-            }
-        });
+        const errors = this.storeValidation(call);
+        if (errors.length) {
+            res.status(400).json(errors);
+        } else {
+            const sql = 'INSERT INTO calls SET ?;';
+    
+            conn.query(sql, call, (err, results) => {
+                if (err) {
+                    res.status(400).json(err);
+                } else {
+                    res.status(201).json(results);
+                }
+            });
+        }
+    }
+
+    storeValidation(call) {
+        const validClient = call.client.length >= 4;
+        const validDate = moment().isSameOrAfter(call.created_at);
+
+        const validation = [
+            {
+                name: 'client',
+                valid: validClient,
+                message: 'O cliente deve ter pelo menos 4 caracteres',
+            },
+            {
+                name: 'date',
+                valid: validDate,
+                message: 'A data deve ser maior ou igual que a data atual',
+            },
+        ];
+
+        return validation.filter(field => !field.valid);
     }
 }
 
