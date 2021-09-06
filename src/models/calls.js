751728d9eac1053a7f4a1627086ca193d4b8754e
ronaldemanuel <emronald15el@gmail.com>
2021-09-06 15:54:54 -0300

    feat: format dates

--- src/models/calls.js
+++ src/models/calls.js
@@ -1,7 +1,14 @@
+const moment = require('moment');
 const conn = require('../database/connection');
 
 class Calls {
-    store(call) {
+    store(requestCall) {
+        const created_at = moment().format('YYYY-MM-DD HH:MM:SS');
+        const date = moment(requestCall.date, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
+
+        console.log(created_at);
+        const call = {...requestCall, created_at, date};
+        
         const sql = 'INSERT INTO calls SET ?;';
 
         conn.query(sql, call, (err, res) => {
